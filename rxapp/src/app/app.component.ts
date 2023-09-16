4f68b36b30ad0f7368428da038db1a5f74c28b6c
SSG <gssenyonjo@gmail.com>
2023-09-16 19:00:43 +0300

    implemented changing title after 2 seconds using callbacks

--- rxapp/src/app/app.component.ts
+++ rxapp/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rxapp';
+    title = 'rxapp';
+
+    private setTitle = () => {
+        this.title = 'second title'
+    }
+
+    private changeTitle(callback: Function) {
+        setTimeout(() => {
+            callback()
+        }, 2000)
+    }
+
+    constructor() {
+        this.changeTitle(this.setTitle)
+    }
+    
 }
