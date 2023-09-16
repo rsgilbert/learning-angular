889a6165533972fb374c7d81a666abfe1717793b
SSG <gssenyonjo@gmail.com>
2023-09-16 06:47:46 +0300

    created and used numeric directive

--- hello/src/app/app.module.ts
+++ hello/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductsModule } from './products/products.module';
 import { CartComponent } from './cart/cart.component';
+import { NumericDirective } from './numeric.directive';
 // import { CopyrightDirective } from './copyright.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NumericDirective,
     // CopyrightDirective, 
   ],
   imports: [
---
+++ hello/src/app/numeric.directive.ts
@@ -0,0 +1,28 @@
+import { Directive, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appNumeric]'
+})
+export class NumericDirective {
+
+    @HostBinding('class') currentClass = ''
+
+    @HostListener('keypress', ['$event'])
+    onKeyPress(event: KeyboardEvent) {
+        const charCode = event.key.charCodeAt(0)
+        console.log({ charCode })
+        console.dir(event)
+        // charcodes
+        // '0': 48 , '9': 57
+        if(charCode > 31 && (charCode < 48 || charCode > 57)) {
+            this.currentClass = 'invalid'
+            event.preventDefault()
+        }
+        else {
+            this.currentClass = 'valid'
+        }
+    }
+
+  constructor() { }
+
+}
