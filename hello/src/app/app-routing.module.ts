004bec1eda890acf9358825d64216d6b77e4f235
SSG <gssenyonjo@gmail.com>
2023-09-17 21:57:15 +0300

    looked at styling based on active route

--- hello/src/app/app-routing.module.ts
+++ hello/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router'
 import { ProductListComponent } from './products/product-list/product-list.component';
 import { CartComponent } from './cart/cart.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
     { path: 'cart', component: CartComponent },
-    { path: '**', component: CartComponent }
+    { path: '**', component: PageNotFoundComponent }
 ]
 
 @NgModule({
--- hello/src/app/products/products-routing.module.ts
+++ hello/src/app/products/products-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { ProductListComponent } from './product-list/product-list.component'
+import { PageNotFoundComponent } from '../page-not-found/page-not-found.component'
 
 
-const ROUTES : Routes = [
+const ROUTES: Routes = [
     { path: 'products', component: ProductListComponent },
+    { path: '', redirectTo: '/products', pathMatch: 'full' }
 ]
 
 @NgModule({
